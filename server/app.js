e9c1b5388ae636e938458728d65293c7ac5ef3f9
patricio <patoare@hotmail.com>
2025-01-14 18:07:53 +0100

    day2 done

--- server/app.js
+++ server/app.js
@@ -7,10 +7,15 @@ const students = require("./students.json");
 const PORT = 5005;
 const cors = require("cors");
 
+
 // STATIC DATA
 // Devs Team - Import the provided files with JSON data of students and cohorts here:
 // ...
-
+const mongoose = require("mongoose");
+mongoose
+  .connect("mongodb://127.0.0.1:27017/mongoose-intro-dev")
+  .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
+  .catch(err => console.error("Error connecting to mongo", err));
 // INITIALIZE EXPRESS APP - https://expressjs.com/en/4x/api.html#express
 const app = express();
 
@@ -45,6 +50,9 @@ app.get("/api/students", (req, res) => {
 app.use((req, res) => {
   res.status(404).json({ message: "Route not found" });
 });
+
+
+
 
 // START SERVER
 app.listen(PORT, () => {
---
+++ server/models/Student.model.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// CREATE A SCHEMA
+const studentSchema = new Schema({
+
+    firstName: {type: String, required: true},
+    lastName: {type: String, required: true},
+    email: { type: String, required: true, unique: true},
+    phone: { type: String, required: true},
+    linkedinUrl: { type: String, Default: ""},
+    languages: { type: [String],  enum: ["English", "Spanish", "French", "German", "Portuguese", "Dutch", "Other"]},
+    program: { type: String, enum: ["Web Dev", "UX/UI", "Data Analytics", "Cybersecurity"]},
+    background: { type: String, default: ""},
+    image: { type: String, Default: 'https://i.imgur.com/r8bo8u7.png'},
+    cohort: { type: ObjectId },
+    projects: { type: [String] }
+
+});
+
+// CREATE A MODEL
+const Student = mongoose.model("Student", recipeSchema);
+
+// EXPORT THE MODEL
+module.exports = Student;
